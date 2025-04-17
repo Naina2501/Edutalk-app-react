83199c0f0ad4e34c6b74f780442a844aee17d6b5
Naina <nainabahuriya257@gmail.com>
2025-04-17 17:42:52 +0530

    Update in Navigation

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -21,8 +21,8 @@ const Navbar = () => {
             <li><Link to="hero" smooth={true} offset={0} duration={500}>Home</Link></li>
             <li><Link to="programs" smooth={true} offset={-260} duration={500}>Program</Link></li>
             <li><Link to="about" smooth={true} offset={-150} duration={500}>About-us</Link></li>
-            <li><Link to="testimonials" smooth={true} offset={-260} duration={500}>Campus</Link></li>
-            <li><Link to="campus" smooth={true} offset={-150} duration={500}>About-us</Link></li>
+            <li><Link to="testimonials" smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+            <li><Link to="campus" smooth={true} offset={-150} duration={500}>Campus</Link></li>
             <li className='mt-3'><Link className='nav-btn text-decoration-none ' to="contact" smooth={true} offset={0} duration={500} >Contact</Link></li>
         </ul>
         <div className='menu-icon bi bi-list' onClick={toggleMenu}></div>
